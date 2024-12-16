bd8e55a3a6228bf26ba1261a9e66a325fca624d5
Elif Ezgi Ekinci <eezgiekinci@outlook.com>
2024-12-16 15:26:03 +0300

    modify : index

--- cnn_image.py
+++ cnn_image.py
@@ -1,4 +1,5 @@
 import argparse
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
