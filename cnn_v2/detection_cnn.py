dadbfb6b5e7d628e0c8be734d0d59c9721434e3b
Kerem Başçiftçi <kerem.bascifci@gazi.edu.tr>
2024-12-16 22:38:08 +0300

    path and imagedatagenerator method

--- cnn_v2/detection_cnn.py
+++ cnn_v2/detection_cnn.py
@@ -4,3 +4,12 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropou
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 import os
+
+data_dir = "C:\Users\kerem\OneDrive\Masaüstü\data"  
+img_height, img_width = 128, 128  
+batch_size = 32
+
+datagen = ImageDataGenerator(
+    rescale=1.0/255,           
+    validation_split=0.2    
+)
