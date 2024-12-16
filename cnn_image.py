ec2f7984b9eb6759a0fc2f363263c0d095a99d09
Elif Ezgi Ekinci <eezgiekinci@outlook.com>
2024-12-16 14:09:36 +0300

    Add CNN image processing script

---
+++ cnn_image.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
+
