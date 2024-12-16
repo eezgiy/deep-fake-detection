eb2bf30c35d69648544d28971d413f96f9a0ae18
Kerem Başçiftçi <kerem.bascifci@gazi.edu.tr>
2024-12-16 22:42:49 +0300

    cnn -kerem

--- cnn_v2/detection_cnn.py
+++ cnn_v2/detection_cnn.py
@@ -29,3 +29,18 @@ val_data = datagen.flow_from_directory(
     class_mode='binary',      
     subset='validation'         
 )
+
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(64, activation='relu'), 
+    Dense(1, activation='softmax'),
+    Adam(learning_rate=0.001),  
+])
